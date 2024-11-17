24accaa4a8d42fc270573448bf2b7ebb972db8bc
pastc <147841049+pastc@users.noreply.github.com>
2024-11-17 23:36:25 +0900

    Add JetBrains Mono font to the font packages

--- system.nix
+++ system.nix
@@ -103,15 +103,14 @@
 
   fonts = {
     packages = with pkgs; [
+      jetbrains-mono
+
       # nerdfonts
       # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/pkgs/data/fonts/nerdfonts/shas.nix
       (nerdfonts.override {
         fonts = [
           # symbols icon only
           "NerdFontsSymbolsOnly"
-
-          # Characters
-          "JetBrainsMono"
         ];
       })
     ];
