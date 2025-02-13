fac1e9f9291896242d71993682c2293218b6fe4e
Tahir Murata <147841049+tahirmurata@users.noreply.github.com>
2025-02-13 21:27:11 +0900

    Use abbreviations instead of functions

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
+    abbr --add lg lazygit
+    abbr --add ll ls -lh
+
     source "$GHOSTTY_RESOURCES_DIR/shell-integration/fish/vendor_conf.d/ghostty-shell-integration.fish"
 end
