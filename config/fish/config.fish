785d9660d90fb5dd30be4521d6d10d9ef3dcb618
Tahir Murata <147841049+tahirmurata@users.noreply.github.com>
2025-02-13 20:04:56 +0900

    Add fish config

---
+++ config/fish/config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+    source "$GHOSTTY_RESOURCES_DIR/shell-integration/fish/vendor_conf.d/ghostty-shell-integration.fish"
+end
