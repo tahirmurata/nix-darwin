37353872c4a8ce19338a0f66b5bc176c71ac19b7
Tahir Murata <147841049+tahirmurata@users.noreply.github.com>
2025-02-14 11:41:00 +0900

    Remove fish greeting message

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,6 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
+    # Remove fish_greeting
+    set -g fish_greeting
+
     abbr --add lg lazygit
 
     source "$GHOSTTY_RESOURCES_DIR/shell-integration/fish/vendor_conf.d/ghostty-shell-integration.fish"
