fb5766259b1f421fc826fde0173fa50173d23753
Tahir Murata <147841049+tahirmurata@users.noreply.github.com>
2025-02-14 11:41:21 +0900

    Add check if it is run in ghostty

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,5 +6,8 @@ if status is-interactive
 
     abbr --add lg lazygit
 
-    source "$GHOSTTY_RESOURCES_DIR/shell-integration/fish/vendor_conf.d/ghostty-shell-integration.fish"
+    # Ghostty manual shell integration
+    if test -n "$GHOSTTY_RESOURCES_DIR"
+        source "$GHOSTTY_RESOURCES_DIR/shell-integration/fish/vendor_conf.d/ghostty-shell-integration.fish"
+    end
 end
