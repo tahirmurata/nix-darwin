0424aaf87551560fc56e9bbe66bde2a08e7de07b
Tahir Murata <147841049+tahirmurata@users.noreply.github.com>
2025-05-11 11:15:37 +0900

    Update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,6 +6,8 @@ if status is-interactive
 
     abbr --add lg lazygit
 
+    set -qU XDG_CONFIG_HOME; or set -Ux XDG_CONFIG_HOME $HOME/.config
+
     # Ghostty manual shell integration
     if test -n "$GHOSTTY_RESOURCES_DIR"
         source "$GHOSTTY_RESOURCES_DIR/shell-integration/fish/vendor_conf.d/ghostty-shell-integration.fish"
